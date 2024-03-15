197cf117864ae6a8e8073994eab67a03b385e75c
Selectel Git User <git-user@selectel.ru>
2024-03-15 13:45:32 +0700

    pr1

---
+++ ConsoleApp129/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp129
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+            Map map = new Map();
+            map.Map_generation();
+            ConsoleKeyInfo key = Console.ReadKey();
+
+            while (key.Key != ConsoleKey.Escape)
+                {
+                map.Drawing_the_map();
+                key = Console.ReadKey();
+                map.MovePersons(key.Key);
+                map.MovePersons();
+                Console.Clear();
+            }
+        }
+    }
+}
+            
